2967e4a3eff21e485481a4155d8f6a34e627e8ce
ErwinNanyaro <erwinsebastian320@gmail.com>
2025-01-31 17:30:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+// Fetch locations from the backend
+fetch('http://127.0.0.1:5000/locations')
+    .then(response => response.json())
+    .then(data => {
+        const fromSelect = document.getElementById('from');
+        const toSelect = document.getElementById('to');
+        data.forEach(location => {
+            const option = document.createElement('option');
+            option.value = location.LocationID;
+            option.text = location.LocationName;
+            fromSelect.add(option);
+            toSelect.add(option.cloneNode(true));
+        });
+    });
 
+// Fetch routes and prices
+function getRoutes() {
+    const from = document.getElementById('from').value;
+    const to = document.getElementById('to').value;
+    fetch(`http://127.0.0.1:5000/routes?from=${from}&to=${to}`)
+        .then(response => response.json())
+        .then(data => {
+            const resultsDiv = document.getElementById('results');
+            resultsDiv.innerHTML = '';
+            data.forEach(route => {
+                resultsDiv.innerHTML += `
+                    <p>Motorcycle: ${route.MotorcyclePrice} TZS</p>
+                    <p>Bajaji: ${route.BajajiPrice} TZS</p>
+                    <p>Car: ${route.CarPrice} TZS</p>
+                `;
+            });
+        });
+}
