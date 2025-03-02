806491070a8707dc8d48bcd5ddb044c33a4b6ac2
ErwinNanyaro <erwinsebastian320@gmail.com>
2025-03-02 11:55:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function getDeviceToken() {
 
 // Function to register the device token
 function registerDeviceToken(deviceToken) {
-    const riderPhone = '0628284454'; // Replace with the rider's phone number
+    const riderPhone = '754996969'; // Replace with the rider's phone number
     fetch('http://192.168.1.122:5000/register-device-token', {
         method: 'POST',
         headers: {
